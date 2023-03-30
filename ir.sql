10934b7647d4603ee4466b052cf512ef90900e89
mrfadil22 <122786329+mrfadil22@users.noreply.github.com>
2023-03-30 14:50:06 +0700

    Add files via upload

---
+++ ir.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 30 Mar 2023 pada 05.34
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `skripsispkk`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `ir`
+--
+
+CREATE TABLE `ir` (
+  `jumlah` int(11) NOT NULL,
+  `nilai` float NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `ir`
+--
+
+INSERT INTO `ir` (`jumlah`, `nilai`) VALUES
+(1, 0),
+(2, 0),
+(3, 0.58),
+(4, 0.9),
+(5, 1.12),
+(6, 1.24),
+(7, 1.32),
+(8, 1.41),
+(9, 1.45),
+(10, 1.49),
+(11, 1.51),
+(12, 1.48),
+(13, 1.56),
+(14, 1.57),
+(15, 1.59);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `ir`
+--
+ALTER TABLE `ir`
+  ADD PRIMARY KEY (`jumlah`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
